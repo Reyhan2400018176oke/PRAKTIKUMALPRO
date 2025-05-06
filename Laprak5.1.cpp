edef2a8466dccc16d3f755f108aee44eb326eff4
Reyhan Alfandi Arya Putra <reyhan.aryaputra@gmail.com>
2025-05-06 13:14:38 +0700

    praktikum5

---
+++ Laprak5.1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class perulangan{
+	public:
+		perulangan(){
+			n=4;
+		}
+		void ulang_rekursif(int);
+	private:
+		int n;
+};
+
+void perulangan::ulang_rekursif(int n){
+	if (n==0)
+		return;
+	cout<<n<<" ";
+	ulang_rekursif(n-1);
+}
+
+int main(){
+	perulangan x;
+	cout<<"menggunakan cara rekursif : ";
+	x.ulang_rekursif(4);
+}
